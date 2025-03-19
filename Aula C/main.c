6e79fda95e535fb2441a564cb96af40bd51e5a25
Pedro Henrique Freire de Oliveira <pedro.freireo@icloud.com>
2025-03-19 19:37:48 -0300

    Initial Commit

---
+++ Aula C/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Aula C
+//
+//  Created by Pedro Henrique Freire de Oliveira on 19/03/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
