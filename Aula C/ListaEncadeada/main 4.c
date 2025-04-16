49b2a6c365a60448744742426724a6812ab1bb1d
Pedro Henrique Freire de Oliveira <pedrobolina123@gmail.com>
2025-04-16 16:41:17 -0300

    Lista encadeada explicação 1

---
+++ Aula C/ListaEncadeada/main 4.c
@@ -0,0 +1,44 @@
+//
+//  main.c
+//  Aula C
+//
+//  Created by Pedro Henrique Freire de Oliveira on 19/03/25.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   typedef struct nodo{
+       char data;
+       struct nodo *link;
+   } *ptr;
+   
+   ptr lista = malloc(sizeof(struct nodo));
+   lista->data = 'C';
+   
+   ptr aux = malloc(sizeof(struct nodo));
+   lista->link = aux;
+   
+   aux->data = 'A';
+   aux->link = malloc(sizeof(struct nodo));
+   aux = aux->link;
+
+   aux->data = 'D';
+   aux->link = malloc(sizeof(struct nodo));
+   aux = aux->link;
+
+   aux->data = 'E';
+   aux->link = NULL;
+
+   // Impressão da lista encadeada
+   ptr temp = lista;
+   while (temp != NULL) {
+       printf("%c", temp->data);
+       temp = temp->link;
+   }
+   printf("\nNULL\n");
+
+   return 0;
+}
